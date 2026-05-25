c80db96cb18df14536e0f172711ab07bd7bcd1c4
Amir Awak <amir.kian@awak.ch>
2026-05-25 17:44:32 +0200

    add doc view

--- tracker-app/src/app/app.routes.ts
+++ tracker-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { UploadPage } from './pages/upload/upload.page';
 import { DashboardPage } from './pages/dashboard/dashboard.page';
+import { DocumentViewPage } from './pages/document-view/document-view.page';
 
 export const routes: Routes = [
   { path: '', component: DashboardPage },
   { path: 'upload', component: UploadPage },
-  { path: '**', redirectTo: '' }
+  { path: 'documents/:id', component: DocumentViewPage },
+  { path: '**', redirectTo: '' },
 ];
---
+++ tracker-app/src/app/pages/document-view/document-view.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-view',
+  imports: [],
+  templateUrl: './document-view.page.html',
+  styleUrl: './document-view.page.scss',
+})
+export class DocumentViewPage {}
